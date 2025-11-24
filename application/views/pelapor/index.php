788a3b0f2182f8d2237011bd82fae7275362fd4c
Alvyn Wira Pratama <168216543+alvynpratama@users.noreply.github.com>
2025-11-24 12:07:52 +0700

    Update index.php

--- application/views/pelapor/index.php
+++ application/views/pelapor/index.php
@@ -1,70 +1,93 @@
-<?php 
-    $proses = $this->db->get_where('pengaduan', ['status_pengaduan' => 'proses'])->num_rows();
-    $valid = $this->db->get_where('pengaduan', ['status_pengaduan' => 'valid'])->num_rows();
-    $pengerjaan = $this->db->get_where('pengaduan', ['status_pengaduan' => 'pengerjaan'])->num_rows();
-    $selesai = $this->db->get_where('pengaduan', ['status_pengaduan' => 'selesai'])->num_rows();
-    $tidak_valid = $this->db->get_where('pengaduan', ['status_pengaduan' => 'tidak_valid'])->num_rows();
-    $belum_ditanggapi = $this->db->get_where('pengaduan', ['status_pengaduan' => 'belum_ditanggapi'])->num_rows();
-?>
-
-
 <div class="container">
     <div class="row justify-content-center py-3">
         <div class="col-lg">
             <h3><i class="fas fa-fw fa-tachometer-alt"></i> Dasbor</h3>
         </div>
     </div>
+
     <div class="row my-3">
+        
         <div class="col-lg-3">
             <div class="card shadow">
                 <div class="card-body">
                     <h5><i class="fas fa-fw fa-times"></i> Belum ditanggapi</h5>
-                    <h6 class="text-muted mt-3">Jumlah data: <span class="bg-info py-1 px-2 rounded"><?= $belum_ditanggapi; ?></span></h6>
+                    <h6 class="text-muted mt-3">Jumlah data: 
+                        <span class="bg-info py-1 px-2 rounded">
+                            <?= count($pengaduan_belum_ditanggapi_pelapor); ?>
+                        </span>
+                    </h6>
                 </div>
             </div>
         </div>
+
         <div class="col-lg-3">
             <div class="card shadow">
                 <div class="card-body">
                     <h5><i class="fas fa-fw fa-sync"></i> Proses</h5>
-                    <h6 class="text-muted mt-3">Jumlah data: <span class="bg-info py-1 px-2 rounded"><?= $proses; ?></span></h6>
+                    <h6 class="text-muted mt-3">Jumlah data: 
+                        <span class="bg-info py-1 px-2 rounded">
+                            <?= count($pengaduan_proses_pelapor); ?>
+                        </span>
+                    </h6>
                 </div>
             </div>
         </div>
+
         <div class="col-lg-3">
             <div class="card shadow">
                 <div class="card-body">
                     <h5><i class="fas fa-fw fa-check"></i> Valid</h5>
-                    <h6 class="text-muted mt-3">Jumlah data: <span class="bg-info py-1 px-2 rounded"><?= $valid; ?></span></h6>
+                    <h6 class="text-muted mt-3">Jumlah data: 
+                        <span class="bg-info py-1 px-2 rounded">
+                            <?= count($pengaduan_valid_pelapor); ?>
+                        </span>
+                    </h6>
                 </div>
             </div>
         </div>
+
         <div class="col-lg-3">
             <div class="card shadow">
                 <div class="card-body">
                     <h5><i class="fas fa-fw fa-hammer"></i> Pengerjaan</h5>
-                    <h6 class="text-muted mt-3">Jumlah data: <span class="bg-info py-1 px-2 rounded"><?= $pengerjaan; ?></span></h6>
+                    <h6 class="text-muted mt-3">Jumlah data: 
+                        <span class="bg-info py-1 px-2 rounded">
+                            <?= count($pengaduan_pengerjaan_pelapor); ?>
+                        </span>
+                    </h6>
                 </div>
             </div>
         </div>
+
         <div class="col-lg-3">
             <div class="card shadow">
                 <div class="card-body">
                     <h5><i class="fas fa-fw fa-check-double"></i> Selesai</h5>
-                    <h6 class="text-muted mt-3">Jumlah data: <span class="bg-info py-1 px-2 rounded"><?= $selesai; ?></span></h6>
+                    <h6 class="text-muted mt-3">Jumlah data: 
+                        <span class="bg-info py-1 px-2 rounded">
+                            <?= count($pengaduan_selesai_pelapor); ?>
+                        </span>
+                    </h6>
                 </div>
             </div>
         </div>
+
         <div class="col-lg-3">
             <div class="card shadow">
                 <div class="card-body">
                     <h5><i class="fas fa-fw fa-times"></i> Tidak Valid</h5>
-                    <h6 class="text-muted mt-3">Jumlah data: <span class="bg-info py-1 px-2 rounded"><?= $tidak_valid; ?></span></h6>
+                    <h6 class="text-muted mt-3">Jumlah data: 
+                        <span class="bg-info py-1 px-2 rounded">
+                            <?= count($pengaduan_tolak_pelapor); ?>
+                        </span>
+                    </h6>
                 </div>
             </div>
         </div>
+
     </div>
     <hr>
+
     <div class="row my-3">
         <div class="col-lg">
             <h4><i class="fas fa-fw fa-times"></i> Laporan yang belum ditanggapi</h4>
